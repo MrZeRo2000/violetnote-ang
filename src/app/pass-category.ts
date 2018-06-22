360dd5bb51d35a1a59021583a5061f62f5a3762d
MrZeRo2000 <MrZeRo2000@mail.ru>
2018-06-22 17:33:03 +0300

    added services and data model items

---
+++ src/app/pass-category.ts
@@ -0,0 +1,3 @@
+export class PassCategory {
+  categoryName: String;
+}
---
+++ src/app/pass-note.ts
@@ -0,0 +1,11 @@
+import {PassCategory} from './pass-category';
+
+export class PassNote {
+  passCategory: PassCategory;
+  system: String;
+  user: String;
+  password: String;
+  comments: String;
+  custom: String;
+  info: String;
+}
