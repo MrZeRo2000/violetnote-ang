1f2626fcbed43952240ce8b3e7131afe036cd36b
MrZeRo2000 <MrZeRo2000@mail.ru>
2018-07-17 15:14:54 +0300

    added environment dependent build: remove unneeded assets, added script for build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  VERSION: require('../../package.json').version,
   production: true,
   passDataUrl: 'http://localhost:8080/violetnote-ws/rest/readdata',
   loadDelay: 0
