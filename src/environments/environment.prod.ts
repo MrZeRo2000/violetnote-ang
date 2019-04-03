547e7cc904d5705927942df95bf2b05a233d9b35
MrZeRo2000 <MrZeRo2000@mail.ru>
2019-04-03 16:54:50 +0300

    changed PROD url for passdata service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   VERSION: require('../../package.json').version,
   production: true,
-  passDataUrl: 'http://localhost:8080/violetnote-ws/rest/readdatapost',
+  passDataUrl: 'http://localhost:8080/violetnote-wss/passdata',
   loadDelay: 0
 };
