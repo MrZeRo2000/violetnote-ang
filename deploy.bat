9e34ec70ef5675cf8fd629d570ffd5db07ed7129
MrZeRo2000 <MrZeRo2000@mail.ru>
2020-01-05 12:37:50 +0200

    update to latest angular and ngx-bootstrap

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 CALL buildprod.bat
-RMDIR /S /Q D:\prj\apache-tomcat-8.0.30\work\Catalina\localhost\violetnote-ang\
-RMDIR /S /Q D:\prj\apache-tomcat-8.0.30\webapps\violetnote-ang\
-XCOPY dist\violetnote-ang\*.* D:\prj\apache-tomcat-8.0.30\webapps\violetnote-ang\
+RMDIR /S /Q D:\prj\apache-tomcat-9.0.14\work\Catalina\localhost\violetnote-ang\
+RMDIR /S /Q D:\prj\apache-tomcat-9.0.14\webapps\violetnote-ang\
+XCOPY dist\violetnote-ang\*.* D:\prj\apache-tomcat-9.0.14\webapps\violetnote-ang\
