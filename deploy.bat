db0021919177dcdf85c58fd4dd897ad792bf8235
MrZeRo2000 <MrZeRo2000@mail.ru>
2020-01-14 15:21:41 +0200

    fixed deployment target

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 CALL buildprod.bat
-RMDIR /S /Q D:\prj\apache-tomcat-9.0.14\work\Catalina\localhost\violetnote-ang\
-RMDIR /S /Q D:\prj\apache-tomcat-9.0.14\webapps\violetnote-ang\
-XCOPY dist\violetnote-ang\*.* D:\prj\apache-tomcat-9.0.14\webapps\violetnote-ang\
+RMDIR /S /Q D:\prj\apache-tomcat-9.0.24\work\Catalina\localhost\violetnote-ang\
+RMDIR /S /Q D:\prj\apache-tomcat-9.0.24\webapps\violetnote-ang\
+XCOPY dist\violetnote-ang\*.* D:\prj\apache-tomcat-9.0.24\webapps\violetnote-ang\
