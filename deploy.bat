69cfa72de7ed48b6cdc2a2517cc271accaedf1ba
MrZeRo2000 <MrZeRo2000@mail.ru>
2020-06-11 16:57:22 +0300

    changed deploy script, version 0.1.0

--- deploy.bat
+++ deploy.bat
@@ -2,3 +2,4 @@ CALL buildprod.bat
 RMDIR /S /Q D:\prj\apache-tomcat-9.0.24\work\Catalina\localhost\violetnote-ang\
 RMDIR /S /Q D:\prj\apache-tomcat-9.0.24\webapps\violetnote-ang\
 XCOPY dist\violetnote-ang\*.* D:\prj\apache-tomcat-9.0.24\webapps\violetnote-ang\
+XCOPY dist\violetnote-ang\assets\env.json D:\prj\apache-tomcat-9.0.24\webapps\violetnote-ang\assets\
