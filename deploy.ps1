864d53438ae704812afaf97036f708cc68c01aff
MrZeRo2000 <MrZeRo2020@outlook.com>
2024-10-30 14:51:10 +0200

    Improved deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -1,1 +1,2 @@
-Invoke-Expression "../common/builder/deployWebProject.ps1 violetnote-ang"
+$folderName = Split-Path -Path $PSScriptRoot -Leaf
+Invoke-Expression "../common/builder/deployWebProject.ps1 $folderName"
