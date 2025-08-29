26666cef93342f9e1c4e5aaa2bcfb008c24f0ea3
MrZeRo2000 <MrZeRo2020@outlook.com>
2025-08-29 10:11:05 +0300

    PassDataSearchNote: implemented sorting

--- src/app/components/pass-data-search-note-list/pass-data-search-note-list.ts
+++ src/app/components/pass-data-search-note-list/pass-data-search-note-list.ts
@@ -1,4 +1,4 @@
-import {Component, inject, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, inject, signal, ViewChild} from '@angular/core';
 import { PassDataSearchService } from "../../services/pass-data-search-service";
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatSort, MatSortModule} from '@angular/material/sort';
@@ -20,27 +20,50 @@ import {AsyncPipe} from '@angular/common';
   templateUrl: './pass-data-search-note-list.html',
   styleUrl: './pass-data-search-note-list.scss'
 })
-export class PassDataSearchNoteList {
+export class PassDataSearchNoteList implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
   private readonly dialog = inject(MatDialog);
   private passDataSearchService = inject(PassDataSearchService)
 
-  displayedColumns: string[] = ['category', 'system', 'user', 'url'];
+  displayedColumns: string[] = ['categoryName', 'system', 'user', 'url'];
 
-  public dataSource: MatTableDataSource<PassDataSearchResult> = new MatTableDataSource<PassDataSearchResult>();
+  private sortDataAccessor(item: PassDataSearchResult, headerId: string): string  {
+      // Check if the headerId matches the dictionary property
+      if (headerId === 'system') {
+      // Return the specific property you want to sort by
+      return item.passNote.system;
+    } else if (headerId === 'user') {
+      return item.passNote.user;
+    } else {
+      return item[headerId as keyof PassDataSearchResult] as string;
+    }
+  };
+
+  private dataSourceSignal = signal<MatTableDataSource<PassDataSearchResult>>(new MatTableDataSource([] as  PassDataSearchResult[]))
+  dataSource = this.dataSourceSignal.asReadonly()
 
   public data$ = this.passDataSearchService.searchResultAction$.pipe(
     tap(data => {
       console.log(`data$: ${JSON.stringify(data)}`);
       if(data) {
-        this.dataSource.data = data
-        this.dataSource.sort = this.sort
-        this.dataSource.paginator = this.paginator
+        const newDataSource = new MatTableDataSource<PassDataSearchResult>(data);
+        newDataSource.sortingDataAccessor = this.sortDataAccessor
+        newDataSource.paginator = this.paginator
+        newDataSource.sort = this.sort
+        this.dataSourceSignal.set(newDataSource)
       }
     })
   )
+
+  ngAfterViewInit(): void {
+    // Initial setup for paginator and sort.
+    // The computed signal will handle updates.
+    this.dataSourceSignal().sortingDataAccessor = this.sortDataAccessor
+    this.dataSourceSignal().paginator = this.paginator;
+    this.dataSourceSignal().sort = this.sort;
+  }
 
   onRowClicked(row: PassDataSearchResult) {
     console.log(`Clicked: ${JSON.stringify(row)}`);
