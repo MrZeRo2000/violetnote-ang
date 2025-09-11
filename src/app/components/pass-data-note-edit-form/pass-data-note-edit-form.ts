6920bba9f1836b7a3455c7976dd26b13c9fc41dd
MrZeRo2000 <MrZeRo2000@mail.ru>
2025-09-11 23:11:14 +0300

    iss#110 VioletNoteMat: edit existing note and save: system control and validation

--- src/app/components/pass-data-note-edit-form/pass-data-note-edit-form.ts
+++ src/app/components/pass-data-note-edit-form/pass-data-note-edit-form.ts
@@ -1,17 +1,30 @@
 import {Component, inject} from '@angular/core';
 import {MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle} from '@angular/material/dialog';
 import {MatButton} from '@angular/material/button';
-import {FormBuilder, Validators} from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from '@angular/forms';
 import {PassDataService} from '../../services/pass-data-service';
+import {MatError, MatFormField, MatInput, MatLabel} from '@angular/material/input'
 
 @Component({
   selector: 'app-pass-data-note-edit-form',
   imports: [
+    ReactiveFormsModule,
     MatDialogContent,
     MatDialogTitle,
     MatButton,
     MatDialogActions,
-    MatDialogClose
+    MatDialogClose,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatError,
   ],
   templateUrl: './pass-data-note-edit-form.html',
   styleUrl: './pass-data-note-edit-form.scss'
@@ -21,8 +34,19 @@ export class PassDataNoteEditForm {
   passDataService = inject(PassDataService);
 
   editForm = this.fb.group({
-    systemControl: ['', [Validators.required, Validators.minLength(2),]]
+    systemControl: ['', [Validators.required, this.minLengthTrimmedValidator(2),]]
   })
+
+  minLengthTrimmedValidator(minLength: number): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value.trim()
+      if (!value || (value.length >= minLength)) {
+        return null;
+      }
+      console.log('Validator returning error')
+      return {'minLength': value.length};
+    };
+  }
 
   onSave(): void {
 
