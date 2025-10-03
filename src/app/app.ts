4d7bc0d5026bbe92b2cf1dda98878f3b7a5c4ab1
MrZeRo2000 <MrZeRo2000@mail.ru>
2025-10-03 22:22:32 +0300

    Fixed app name, version 1.0.4

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,5 +9,5 @@ import {Header} from './components/header/header';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('violetnote-mat-ang');
+  protected readonly title = signal('violetnote-ang');
 }
